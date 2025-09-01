924d388aed7f00fb7d4cba6b4b3330da9f6a541b
Código Guerreiro <josuelimasilva24@gmail.com>
2025-09-01 15:04:20 -0300

    aula1
    Primeira aula do curso

---
+++ aula1/index.js
@@ -0,0 +1,5 @@
+console.log('josué de liam silva'); // string = texto
+console.log('"josué de lima silva"');// number = número
+console.log(123456,15.85);
+console.log(35,15,45.,"josue de liam silva")
+
